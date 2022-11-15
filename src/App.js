2d84e0700af930fe388b52d1309882f91358608d
Григорий Жидок(РабочийПК) <zhidokGD@gmail.com>
2022-11-15 18:59:05 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
+import { Canvas } from "@react-three/fiber";
+import { SceneContainer } from "./components/SceneContainer";
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Canvas>
+            <SceneContainer />
+        </Canvas>
+    );
 }
 
 export default App;
